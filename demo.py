67853f087f47fcbece11c0812ef0b06101f2827a
chetanakaushik <42596334+chetanakaushik@users.noreply.github.com>
2018-12-11 02:29:14 -0500

    Update demo.py

--- demo.py
+++ demo.py
@@ -1,1 +1,14 @@
+import os, slackclient
+
+VALET_SLACK_NAME = os.environ.get('SLACK_BOT_NAME')
+VALET_SLACK_TOKEN = os.environ.get('SLACK_BOT_TOKEN')
+# initialize slack client
+valet_slack_client = slackclient.SlackClient(VALET_SLACK_TOKEN)
+# check if everything is alright
+print(VALET_SLACK_NAME)
+print(VALET_SLACK_TOKEN)
+is_ok = valet_slack_client.api_call("users.list").get('ok')
+print(is_ok)
+
 print ("hello world")
+
