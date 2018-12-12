46fbc4f8c3a3fe5bb5b34435d8627f374f9ea6ae
chetanakaushik <42596334+chetanakaushik@users.noreply.github.com>
2018-12-12 12:14:16 -0500

    Update cumminschatbot.py

--- cumminschatbot.py
+++ cumminschatbot.py
@@ -127,10 +127,6 @@ def handle_message(message, user, channel):
     elif is_report(message):
         user_mention = get_mention(user)
         post_message(message=say_report(user_mention), channel=channel)
-
-    # elif say_start(message):
-    #     user_mention = get_mention(user)
-    #     post_message(message=say_getHelp(user_mention), channel=channel)
     else:
         user_mention = get_mention(user)
         post_message(message=say_apologize(user_mention), channel=channel)
